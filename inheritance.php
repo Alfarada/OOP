dddccc37be3fc5bc00feedd442af61b2cd08c5ee
Alfredo Yepez <alfaradayepez@gmail.com>
2023-07-25 08:50:07 -0500

    applying the principle of "tell and don't ask"

--- inheritance.php
+++ inheritance.php
@@ -3,39 +3,90 @@
 declare(strict_types = 1);
 ini_set('display_errors', 1);
 
+function show(string $message): void
+{
+    echo "<p>{$message}</p>";
+}
+
 abstract class Unit {
 
-    protected bool $alive = true;
+    protected int|float $hp = 40;
+    protected int $damage = 40;
+    const MINIMUN_HP = 0;
     public function __construct(
         protected string $name
     )
     { }
 
-    public function move(string $direction): void
+    public abstract function attack(Unit $opponent): void;
+
+    public function takeDamage(int|float $damage)
     {
-        echo "<p>{$this->name} avanza hacia $direction</p>";
+        $this->setHp($this->hp - $damage);
+
+        if ($this->hp <= self::MINIMUN_HP) { $this->die(); }
     }
 
-    public abstract function attack($opponent): void;
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    private function setHp(int|float $hp): void
+    {
+        $this->hp = $hp;
+        // terminate script execution if life has reached zero
+        if ($this->hp <= self::MINIMUN_HP) { exit($this->die()); }
+
+        show("{$this->name} ahora tiene {$this->hp} puntos de vida");
+    }
+
+    public function getHp(): int
+    {
+        return $this->hp;
+    }
+
+    public function die(): void
+    {
+        show("{$this->name} ha muerto");
+    }
 }
 
 class Soldier extends Unit {
-    public function attack($opponent): void
+
+    protected int $damage = 30;
+    public function attack(Unit $opponent): void
     {
-        echo "<p>{$this->name} ataca con la espada a $opponent</p>";
+        show("{$this->name} ataca con la espada a {$opponent->getName()}");
+
+        $opponent->takeDamage($this->damage);
+    }
+
+    public function takeDamage(int|float $damage): void
+    {
+        parent::takeDamage($damage / 2);
     }
 }
 
 class Archer extends Unit {
-    public function attack($opponent): void
+
+    protected int $damage = 15;
+    public function attack(Unit $opponent): void
     {
-        echo "<p>{$this->name} lanza una flecha a $opponent</p>";
+        show("{$this->name} lanza una flecha a {$opponent->getName()}");
+
+        $opponent->takeDamage($this->damage);
+    }
+
+    public function takeDamage(int|float $damage): void
+    {
+        parent::takeDamage($damage / 4);
     }
 }
 
-$alfredo = new Soldier('Alfredo');
-//$alfredo->move('hacia el norte');
-$alfredo->attack('Ramm');
+$bob = new Soldier('Bob');
+$tedd = new Archer('Tedd');
 
-$saylon = new Archer('Saylon');
-$saylon->attack('Ramm');
+$bob->attack($tedd);
+$bob->attack($tedd);
+
